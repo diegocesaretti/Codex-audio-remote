c32a18f5010dd8c25427e1f7a1631b93df663b02
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-09-10 19:52:49 -0300

    Match AmbiP Oreo-compatible Android build settings

---
+++ tv-standalone/app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.bwa3d.codextv"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.bwa3d.codextv"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 3
+        versionName = "0.1.2"
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("../../android/tvsatellite/src/main/java")
+            res.srcDirs("../../android/tvsatellite/src/main/res")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+}
