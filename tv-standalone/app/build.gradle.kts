1325d081df8b8185490a2385d40bb8d5a3c4337f
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-09-11 13:33:04 -0300

    Bump optimized TV Satellite to 0.1.5

--- tv-standalone/app/build.gradle.kts
+++ tv-standalone/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.bwa3d.codextv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
     }
 
     sourceSets {
