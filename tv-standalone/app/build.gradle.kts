22de1d054d8903db78e2a40918c87cb2e7e8389b
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-09-11 11:50:55 -0300

    Bump TV Satellite to 0.1.4 on-demand capture

--- tv-standalone/app/build.gradle.kts
+++ tv-standalone/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.bwa3d.codextv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
     }
 
     sourceSets {
